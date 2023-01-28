91e247d516092aa1b69737d23728bd6022923a0c
halilcn <iletisimhalilcan@gmail.com>
2023-01-28 22:35:29 +0300

    update credit card component

--- src/components/credit-card/index.jsx
+++ src/components/credit-card/index.jsx
@@ -1,11 +1,26 @@
-import './../../styles/components/creditCard.scss';
+import { useSelector } from 'react-redux';
 import CreditCardBgImage from './../../assets/credit-card-bg.jpg';
 import VisaImage from './../../assets/visa.png';
-import { useSelector } from 'react-redux';
+
+import './../../styles/components/creditCard.scss';
 
 const CreditCard = () => {
   const isSelectedBackOfCard = useSelector(state => state.card.isSelectedBackOfCard);
   const { number, name, expireDate, cvv } = useSelector(state => state.card);
+
+  const splittedNumber = () =>
+    number.split('').map((n, i) => (
+      <>
+        {n}
+        {(i + 1) % 4 === 0 && <span> </span>}
+      </>
+    ));
+
+  const hiddenCvvNumber = () =>
+    cvv
+      .split('')
+      .map(_ => '*')
+      .join('');
 
   return (
     <div className={`credit-card ${isSelectedBackOfCard && 'credit-card__toggle-face'}`}>
@@ -14,7 +29,7 @@ const CreditCard = () => {
         <div className="credit-card__filter" />
         <div className="credit-card__info-container">
           <img src={VisaImage} className="credit-card__visa-icon" />
-          <div className="credit-card__info credit-card__info--number">{number}</div>
+          <div className="credit-card__info credit-card__info--number">{splittedNumber()}</div>
           <div className="credit-card__other-info">
             <div className="credit-card__info credit-card__info--name">{name}</div>
             <div className="credit-card__info credit-card__info--expire-date">{expireDate}</div>
@@ -26,7 +41,7 @@ const CreditCard = () => {
         <div className="credit-card__filter" />
         <div className="credit-card__info-container">
           <div className="credit-card__black-line" />
-          <div className="credit-card__info credit-card__cvv">{cvv}</div>
+          <div className="credit-card__info credit-card__info--cvv">{hiddenCvvNumber()}</div>
         </div>
       </div>
     </div>
