a10b628892f9bbc41165ca695d54b9b57b46f080
halilcn <iletisimhalilcan@gmail.com>
2023-01-28 20:13:07 +0300

    add error message funct

--- src/components/common/payment-info/PaymentInfo.jsx
+++ src/components/common/payment-info/PaymentInfo.jsx
@@ -1,12 +1,12 @@
 import './../../../styles/components/paymentInfo.scss';
 
 const PaymentInfo = props => {
-  const { title, ...others } = props;
+  const { title, hasError, ...others } = props;
 
   return (
     <div className="payment__info">
       <div className="payment__info-title">{title}</div>
-      <input className="payment__info-input" {...others} />
+      <input className={`payment__info-input ${hasError && 'payment__info-input--has-error'}`} {...others} />
     </div>
   );
 };
--- src/components/payment-modal/index.jsx
+++ src/components/payment-modal/index.jsx
@@ -1,32 +1,94 @@
-import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { cardActions } from '../../store/card';
 import PaymentInfo from '../common/payment-info/PaymentInfo';
 
+const CARD_FIELDS = {
+  NAME: 'name',
+  NUMBER: 'number',
+  EXPIRE_DATE: 'expireDate',
+  CVV: 'cvv'
+};
+
 const PaymentModal = () => {
+  const [fieldsForError, setFieldsForError] = useState([]);
+
   const dispatch = useDispatch();
+  const card = useSelector(state => state.card);
 
   const toggleCreditCardFace = () => {
     dispatch(cardActions.toggleCardFace());
   };
 
+  const handleSetFieldsForError = field => {
+    if (!fieldsForError.includes(field)) setFieldsForError([...fieldsForError, field]);
+  };
+
+  const handleDeleteFieldsForError = field => setFieldsForError([...fieldsForError.filter(f => f !== field)]);
+
   const onChangeHandle = (e, cardInfoName) => dispatch(cardActions.updateCardInformation({ [cardInfoName]: e.target.value }));
+
+  const handleCheckErrorWhenOnBlur = field => {
+    const value = card[field];
+
+    if (value === '') {
+      handleSetFieldsForError(CARD_FIELDS.NAME);
+      return;
+    }
+
+    if (field === CARD_FIELDS.NAME) {
+      if (value.split(' ').length <= 1) {
+        handleSetFieldsForError(field);
+        return;
+      }
+    }
+
+    if (field === CARD_FIELDS.NUMBER) {
+      if (value.length !== 16) {
+        handleSetFieldsForError(field);
+        return;
+      }
+    }
+
+    if (field === CARD_FIELDS.CVV) {
+      if (value.length !== 3) {
+        handleSetFieldsForError(field);
+        return;
+      }
+    }
+
+    handleDeleteFieldsForError(field);
+  };
+
+  const handleHasError = field => fieldsForError.includes(field);
+
+  const handleOnBlurForCvv = () => {
+    toggleCreditCardFace();
+    handleCheckErrorWhenOnBlur(CARD_FIELDS.CVV);
+  };
+
+  const paymentInputParams = fieldName => ({
+    hasError: handleHasError(fieldName),
+    onBlur: () => handleCheckErrorWhenOnBlur(fieldName),
+    onChange: e => onChangeHandle(e, fieldName)
+  });
 
   return (
     <div className="payment__modal">
       <div className="payment__info-container">
-        <PaymentInfo title="Card Holder" type="text" onChange={e => onChangeHandle(e, 'name')} />
+        <PaymentInfo title="Card Holder" type="text" {...paymentInputParams(CARD_FIELDS.NAME)} />
       </div>
       <div className="payment__info-container">
-        <PaymentInfo title="Card Number" type="text" onChange={e => onChangeHandle(e, 'number')} />
+        <PaymentInfo title="Card Number" type="text" {...paymentInputParams(CARD_FIELDS.NUMBER)} />
       </div>
       <div className="payment__info-container payment__info-container--row">
-        <PaymentInfo title="Expiration Date" type="text" onChange={e => onChangeHandle(e, 'expireDate')} />
+        <PaymentInfo title="Expiration Date" type="text" {...paymentInputParams(CARD_FIELDS.EXPIRE_DATE)} />
         <PaymentInfo
           title="CVV"
           type="number"
-          onChange={e => onChangeHandle(e, 'cvv')}
           onFocus={toggleCreditCardFace}
-          onBlur={toggleCreditCardFace}
+          {...paymentInputParams(CARD_FIELDS.CVV)}
+          onBlur={handleOnBlurForCvv}
         />
       </div>
       <div className="payment__submit">Submit</div>
